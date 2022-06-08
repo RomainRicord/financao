95c64e6031cd95a7040530e8e1c37ee159e1cd80
Romain Ricord <romain.ricord.pro@gmail.com>
2022-06-08 10:41:48 +0200

    Réparation formulaire

--- src/Screens/IncomesFScreens.js
+++ src/Screens/IncomesFScreens.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 import { Button, TextInput, View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
 import { Formik, useFormik } from 'formik';
 import dayjs from 'dayjs';
-import { Picker } from 'react-native-web';
+import {Picker} from '@react-native-picker/picker'
+//import { Picker } from 'react-native-web';
 import * as Yup from 'yup';
+import { Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const IncomesFScreen = () => {
-    // const [user, setUser] = useState('')
+     const [user, setUser] = useState('')
     // const [amount, setAmount] = useState('')
     // const [date, setDate] = useState(new Date())
-    // const [category, setCategory] = useState('')
+     const [category, setCategory] = useState('')
     // const [comments, setComments] = useState('')
 
     const UserList = [
@@ -37,12 +42,24 @@ const IncomesFScreen = () => {
         "Autre revenu"
     ]
 
-    const validationIncomes = Yup.object({
-        user: Yup.string().required("Selectionner un bénéficiaire").oneOf(UserList),
-        amount: Yup.number().required(),
-        date: Yup.date().default(() => new Date()),
-        category: Yup.string().required("Selectionner une catégorie").oneOf(CategoryList),
-        comments: Yup.string().required()
+    const validationIncomes = Yup.object().shape({
+        user: Yup
+        .string()
+        .required("Selectionner un bénéficiaire")
+        .oneOf(UserList),
+        amount: Yup
+        .number("Montant invalide !")
+        .required("Mettre un montant"),
+        date: Yup
+        .date('Date invalide !')
+        .default(() => new Date()),
+        category: Yup
+        .string()
+        .required("Selectionner une catégorie")
+        .oneOf(CategoryList),
+        comments: Yup
+        .string("Commentaire invalide !")
+        .required("Commentaire obligatoire !")
     })
 
     const initialValues = {
@@ -52,100 +69,131 @@ const IncomesFScreen = () => {
         category: '',
         comments: ''
     }
+    /*
     const {values, handleBlur, handleChange, handleSubmit} = useFormik ({
         initialValues: { initialValues },
         validationSchema: validationIncomes,
-    //onSubmit: values => alert(values)
-    onSubmit:{handleSubmit}
-    });
+        //onSubmit: values => alert(values)
+        onSubmit:{handleSubmit}
+    });*/
 
 
 
     return (
-
-
+        
         <Formik
             initialValues={initialValues}
             validationSchema={validationIncomes}
             onSubmit={values => console.log(values)}
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
         >
-           
-
-            <View style={styles.container}>
-                <Text style={styles.title}>Ajout Revenus</Text>
-
-                <View style={styles.groupInput}>
-                    <ScrollView>
-                        <Text style={styles.label}>Bénéficiaire</Text>
-                        <Picker
-                            style={styles.input}
-                        >
-                            {UserList.map((item, index) => {
-                                return (<Picker.Item label={item} value={index} key={index} />)
-                            })}
-                        </Picker>
-                        <Text style={styles.label}>Montant</Text>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={handleChange('amount')}
-                        />
-                        <Text style={styles.label}>Date</Text>
-                        <TextInput
-                            style={styles.input}
-                            // value={dayjs(date).format('DD/MM/YYYY')}
-                            onChangeText={handleChange('date')}
-                            onBlur={handleBlur('date')}
-                        />
-                        <Text style={styles.label}>Catégorie</Text>
-                        <Picker
-                            style={styles.input}
-                        // selectedValue={category}
-                        >
-                            {CategoryList.map((item, index) => {
-                                return (<Picker.Item label={item} value={index} key={index} />)
-                            })}
-                        </Picker>
-                        <Text style={styles.label}>Commentaires</Text>
-                        <TextInput
-                            style={styles.comments}                           
-                            onChangeText={handleChange('comments')}
-                            onBlur={handleBlur('comments')}
-                        />
-                        {/* <Button label='Enregistrer' onPress={handleSubmit} /> */}
-                        <Pressable style={[styles.button,{backgroundColor:'blue'}]} onPress={handleSubmit}>
-            <Text style={styles.textbutton}>Enregistrer</Text>
-          </Pressable>
-                    </ScrollView>
-                </View>
-
-
-            </View >
-            
+            {({ handleChange, handleBlur, handleSubmit, values,errors,isValid }) => (
+                <View style={styles.container}>
+                    <Text style={styles.title}>Ajout Revenus</Text>
+                    
+                            <Text style={styles.label}>Bénéficiaire</Text>
+                            <Picker
+                                style={styles.input}
+                                selectedValue={user}
+                                onValueChange={(itemValue, itemIndex) => {
+                                    values.user = itemValue 
+                                    setUser(itemValue)
+                                }}
+                            >
+                                {UserList.map((item, index) => {
+                                    return (<Picker.Item label={item} value={item} key={index} />)
+                                })}
+                            </Picker>
+                            {errors.user &&
+                                <Text style={styles.error}>{errors.user}</Text>
+                            }
+                            <Text style={styles.label}>Montant</Text>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={handleChange('amount')}
+                                onBlur={handleBlur('amount')}
+                                keyboardType="number-pad"
+                            />
+                            {errors.amount &&
+                                <Text style={styles.error}>{errors.amount}</Text>
+                            }
+                            <Text style={styles.label}>Date</Text>
+                            <TextInput
+                                style={styles.input}
+                                // value={dayjs(date).format('DD/MM/YYYY')}
+                                onChangeText={handleChange('date')}
+                                onBlur={handleBlur('date')}
+                                placeholder="JJ/MM/AAAA"
+                            />
+                            {errors.date &&
+                                <Text style={styles.error}>{errors.date}</Text>
+                            }
+                            <Text style={styles.label}>Catégorie</Text>
+                            <Picker
+                                style={styles.input}
+                                selectedValue={category}
+                                onValueChange={(itemValue, itemIndex) => {
+                                    values.category = itemValue 
+                                    setCategory(itemValue)
+                                }}
+                            // selectedValue={category}
+                            >
+                                {CategoryList.map((item, index) => {
+                                    return (<Picker.Item label={item} value={item} key={index} />)
+                                })}
+                            </Picker>
+                            {errors.category &&
+                                <Text style={styles.error}>{errors.category}</Text>
+                            }
+                            <Text style={styles.label}>Commentaires</Text>
+                            <TextInput
+                                style={styles.comments}                           
+                                onChangeText={handleChange('comments')}
+                                onBlur={handleBlur('comments')}
+                            />
+                            {errors.comments &&
+                                <Text style={styles.error}>{errors.comments}</Text>
+                            }
+                            {/* <Button label='Enregistrer' onPress={handleSubmit} /> */}
+                            <Pressable style={[styles.button,{backgroundColor:'blue'}]} onPress={handleSubmit}>
+                                <Text style={styles.textbutton}>Enregistrer</Text>
+                            </Pressable>
+                    
+                </View >
+            )}
         </Formik>
+        
     )
 }
 export default IncomesFScreen
 
 const styles = StyleSheet.create({
     container: {
+        display:'flex',
         flex: 1,
-        backgroundColor: '#FFF',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
     },
     title: {
         fontSize: 30,
-        fontWeight: 'bold'
-
+        fontWeight: 'bold',
+        marginTop:20
     },
     groupInput: {
         flex: 1,
 
     },
+    error:{
+        fontSize:15,
+        color:'red',
+        textAlign:'left',
+        width:300
+    },
     input: {
         height: 40,
         width: 300,
-        marginTop: 10,
+        marginTop: 1,
         borderWidth: 1,
         padding: 10,
         borderRadius: 15,
@@ -154,7 +202,7 @@ const styles = StyleSheet.create({
     comments: {
         height: 100,
         width: 300,
-        marginTop: 10,
+        marginTop: 5,
         borderWidth: 1,
         padding: 10,
         borderRadius: 15,
@@ -164,6 +212,10 @@ const styles = StyleSheet.create({
     label: {
         fontSize: 20,
         marginTop: 20,
+        textAlign:'left',
+        display:'flex',
+        justifyContent:'flex-start',
+        alignItems:'flex-start'
     },
     button:{
         borderRadius:20,
@@ -171,8 +223,8 @@ const styles = StyleSheet.create({
         display:'flex',
         justifyContent:'center',
         alignItems:'center',
-        margin:20,
-        flex:1
+        margin:30,
+        width:200
       },
       textbutton:{
         fontSize:20,
