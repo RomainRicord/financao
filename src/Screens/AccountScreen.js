463a21ecab679d582a31cbbb2d17e9bd7d12b08f
jessylfoh <jessica.voiment2021@campus-eni.fr>
2022-06-08 11:12:12 +0200

    Ajout des écrans test Account et Stat

---
+++ src/Screens/AccountScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const AccountScreen = () => (
+    <View style={{flex: 1, justifyContent:'center',alignItems:'center'}}>
+        <Text style={{ fontSize: 30,fontWeight: 'bold'}}>Page Compte</Text>
+    </View>
+)
+
+export default AccountScreen
---
+++ src/Screens/StatScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const StatScreen = () => (
+    <View style={{flex: 1, justifyContent:'center',alignItems:'center'}}>
+        <Text style={{ fontSize: 30,fontWeight: 'bold'}}>Page Statistiques</Text>
+    </View>
+)
+
+export default StatScreen
