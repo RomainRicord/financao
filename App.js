0c542f57b8b90cd79e35db02a2ba1a8f638ed5f9
EmiFree <emilianpopesco@yahoo.fr>
2022-06-08 11:24:21 +0200

    Auto stash before merge of "master" and "origin/master"

--- App.js
+++ App.js
@@ -8,12 +8,22 @@ import UserListComponent from './src/Components/UserListComponent';
 import TransactionComponent from './src/Components/TransactionComponent';
 import HomeScreen from './src/Screens/HomeScreen';
 import ChartComponent from './src/Components/ChartComponent';
+<<<<<<< Updated upstream
 import IncomesFScreens from './src/Screens/IncomesFScreens';
+=======
+import ContScreen from './src/Screens/ContScreen';
+import IncomesFScreen from './src/Screens/IncomesFScreens';
+
+>>>>>>> Stashed changes
 
 {/* ____________function App_____________ */}   
 function App() {
   return (
+<<<<<<< Updated upstream
     <IncomesFScreens/>    
+=======
+    <IncomesFScreen/>    
+>>>>>>> Stashed changes
   );
 }
 
